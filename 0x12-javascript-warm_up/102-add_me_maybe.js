681eb6744cdc148b89862a344f05e512fda12550
Osagwu Godson <35034202+Bidant@users.noreply.github.com>
2023-03-13 21:40:47 -0300

    Increment Object

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -2,4 +2,20 @@
 exports.addMeMaybe = function (number, theFunction) {
   number += 1;
   theFunction(number);
+};#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
 };
+console.log(myObject);
+
+myObject.incr = function () {
+  myObject.value += 1;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
